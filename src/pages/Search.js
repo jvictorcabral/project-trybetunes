9d10463f57b87ad3621dd1b0660115129b278dba
Joao <joaovictorcgjvcg@gmail.com>
2022-01-17 22:45:21 -0400

    Entrega do requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,53 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      valueName: '',
+      disable: true,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.buttonChange = this.buttonChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({ valueName: target.value }, () => this.buttonChange());
+  }
+
+  buttonChange() {
+    const TWO = 2;
+    const { valueName } = this.state;
+    if (valueName.length < TWO) {
+      this.setState({ disable: true });
+    } else {
+      this.setState({ disable: false });
+    }
+  }
+
   render() {
+    const { valueName, disable } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
+        <form>
+          <input
+            type="text"
+            placeholder="Nome do Artista"
+            value={ valueName }
+            onChange={ this.handleChange }
+            data-testid="search-artist-input"
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ disable }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
